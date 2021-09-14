b804b8cee3f1ead4a98378546d32fe720918a63b
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2021-09-14 11:15:50 -0300

    start login

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -8,6 +8,21 @@ const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
 
   const chat = chats && chats[activeChat];
+  const renderReadReceipts = (message, isMyMessage) =>
+    chat.people.map(
+      (person, index) =>
+        person.last_read === message.id && (
+          <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+              float: isMyMessage ? "right" : "left",
+              backgroundImage:
+                person.person.avatar && `url(${person.person.avatar})`,
+            }}
+          />
+        )
+    );
 
   const renderMessages = () => {
     const keys = Object.keys(messages);
@@ -35,14 +50,14 @@ const ChatFeed = (props) => {
               marginLeft: isMyMessage ? "0px" : "68px",
             }}
           >
-            read-receiptors
+            {renderReadReceipts(message, isMyMessage)}
           </div>
         </div>
       );
     });
   };
 
-  if (!chat) return "Loading...";
+  if (!chat) return <div />;
 
   return (
     <div className="chat-feed">
